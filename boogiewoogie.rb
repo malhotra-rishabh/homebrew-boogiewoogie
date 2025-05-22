d9bbf7d2351b31bc9919a5e7b0d3761b5440cc62
rishabh <rishabh@gmail.com>
2025-05-23 03:28:40 +0530

    Update Homebrew formula to version 1.0.6 and update SHA256 checksum

--- boogiewoogie.rb
+++ boogiewoogie.rb
@@ -1,11 +1,11 @@
 class Boogiewoogie < Formula
   desc "Git profile manager for switching between multiple Git identities"
   homepage "https://github.com/malhotra-rishabh/boogiewoogie"
-  url "https://github.com/malhotra-rishabh/boogiewoogie/archive/refs/tags/v1.0.5.tar.gz" # always remember to change this while updating version
-  sha256 "326cee6baddff99c16c8836bb7b831b1f78378069de7b75d3d20198ca98e7f11"
+  url "https://github.com/malhotra-rishabh/boogiewoogie/archive/refs/tags/v1.0.6.tar.gz" # always remember to change this while updating version
+  sha256 "5b56c88f94c0ebf5704ae13ad8db4d86c674380a7335db525be01adb18df10dd"
   license "MIT"
   head "https://github.com/malhotra-rishabh/boogiewoogie.git", branch: "main"
-  version "1.0.5"
+  version "1.0.6"
 
   livecheck do
     url :stable
