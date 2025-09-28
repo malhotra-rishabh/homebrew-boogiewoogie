457e7e92de5986da6e594458910cd981d87cdd00
Rishabh Malhotra <rishabhmalhotra091@gmail.com>
2025-09-29 00:04:48 +0530

    Update Homebrew formula to version 1.0.10 and corresponding SHA256

--- boogiewoogie.rb
+++ boogiewoogie.rb
@@ -1,11 +1,11 @@
 class Boogiewoogie < Formula
   desc "Git profile manager for switching between multiple Git identities"
   homepage "https://github.com/malhotra-rishabh/boogiewoogie"
-  url "https://github.com/malhotra-rishabh/boogiewoogie/archive/refs/tags/v1.0.9.tar.gz" # always remember to change this while updating version
-  sha256 "fa07542a781c1e144ec46532fc393c1802ff05d3"
+  url "https://github.com/malhotra-rishabh/boogiewoogie/archive/refs/tags/v1.0.10.tar.gz" # always remember to change this while updating version
+  sha256 "b6ce30d664f29d6a2e6c37c836542a3a7e2fc5c4"
   license "MIT"
   head "https://github.com/malhotra-rishabh/boogiewoogie.git", branch: "main"
-  version "1.0.9"
+  version "1.0.10"
 
   livecheck do
     url :stable
